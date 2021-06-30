af459ada33d5823347702302071528d7fd34fc14
ClauCarloni <claudia.carloni@hotmail.com>
2021-06-30 07:54:51 +0200

    Adding file Solutions for lab sql 4.

--- Solutions.sql
+++ Solutions.sql
@@ -1,30 +1,51 @@
--- Get the unique film ratings.
+-- 1. Get film ratings.
+-- SELECT distinct rating
+-- FROM sakila.film;
 
--- Get the unique release years.
+-- 2. Get release years.
+-- SELECT distinct release_year
+-- FROM sakila.film;
 
--- Get all films with the word ARMAGEDDON in the title.
+-- 3. Get all films with ARMAGEDDON in the title.
+-- SELECT * FROM sakila.film
+-- WHERE title regexp 'ARMAGEDDON';
 
--- Get all films with the word APOLLO in the title
+-- 4. Get all films with APOLLO in the title
+-- SELECT * FROM sakila.film
+-- WHERE title regexp 'APOLLO';
 
--- Get all films which title ends with the word APOLLO. 
+-- 5. Get all films which title ends with APOLLO.
+-- SELECT * FROM sakila.film
+-- WHERE title like '%APOLLO';
+-- Another option:
+-- WHERE title regexp 'APOLLO$';
 
--- Get all films with the word DATE in the title.
+-- 6. Get all films with word DATE in the title.
+-- SELECT * FROM sakila.film
+-- WHERE title regexp ('DATE');
 
--- Get the 10 films with the longest title.
+-- Why this query doesnt work?
+-- WHERE title in ('DATE ', ' DATE', ' DATE ');
+-- Another option:
+-- WHERE title like 'DATE%' or title like '% DATE';
 
--- Get the 10 longest films.
+-- 7. Get 10 films with the longest title.
+-- SELECT title, length(title)
+-- FROM sakila.film
+-- ORDER BY length(title) desc
+-- LIMIT 10;
 
--- How many films include Behind the Scenes content?
+-- 8. Get 10 the longest films.
+-- SELECT title, length
+-- FROM sakila.film
+-- ORDER BY length desc
+-- LIMIT 10;
 
--- List films ordered by release year and title in alphabetical order.
+-- 9. How many films include **Behind the Scenes** content?
+-- SELECT count(*) FROM sakila.film
+-- WHERE special_features regexp 'Behind the Scenes';
 
-
-
-
-
-
-
-
-
-
-
+-- 10. List films ordered by release year and title in alphabetical order.
+-- SELECT *
+-- FROM sakila.film
+-- ORDER BY release_year desc, title asc;
